8d11e2841cb9c7dab1d723cdeca5de183638ed5b
Joseph Hart <joseph.hart@improving.com>
2025-01-09 19:30:07 -0600

    adding data streaming example

---
+++ next-demo/src/app/todos-data-streaming/page.tsx
@@ -0,0 +1,40 @@
+import { Skeleton } from "@/components/skeleton";
+import { getTodos } from "@/fake-apis/getTodos";
+import { getUserInfo } from "@/fake-apis/getUserInfo";
+import { Suspense } from "react";
+import { TodoList } from "./todo-list.client";
+
+function TodoListSkeletons() {
+  const skellies = Array.from({ length: 5 }).map((_, i) => (
+    <li
+      key={`skeleton-${i}`}
+      className="flex flex-col gap-2 border rounded mb-4 p-4"
+    >
+      <Skeleton className="h-4 w-12" />
+      <Skeleton className="h-4 w-20" />
+    </li>
+  ));
+
+  return <>{skellies}</>;
+}
+
+export default async function TodosSuspense() {
+  const user = await getUserInfo();
+  const todosStream = getTodos();
+
+  return (
+    <div className="container mx-auto">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl text-center">Demo - Client-side Rendering</h1>
+        {user && <p>Hello, {user.firstName}!</p>}
+      </div>
+
+      <h1 className="text-4xl text-center uppercase">Todo List</h1>
+      <ul>
+        <Suspense fallback={<TodoListSkeletons />}>
+          <TodoList todosStream={todosStream} />
+        </Suspense>
+      </ul>
+    </div>
+  );
+}
---
+++ next-demo/src/app/todos-data-streaming/todo-list.client.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { TodoDto } from "@/fake-apis/getTodos";
+import { format } from "date-fns";
+import { use } from "react";
+
+export function TodoList({ todosStream }: { todosStream: Promise<TodoDto[]> }) {
+  const todos = use(todosStream);
+
+  const todoListItems = todos.map((todo) => (
+    <li key={todo.id} className="flex flex-col gap-2 border rounded mb-4 p-4">
+      <span>{format(todo.timestamp, "MMM d")}</span>
+      <span>{todo.text}</span>
+    </li>
+  ));
+
+  return <>{todoListItems}</>;
+}
